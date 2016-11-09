37dcbf6a7a8b4cafaf1246d7f2c8aa311a77562c
pickknow <sovsov@gmail.com>
2016-11-09 17:02:53 +0800

    2016-11-09

--- 2.60.rkt
+++ 2.60.rkt
@@ -8,19 +8,23 @@
         ((equal? x (car set)) #t)
         (else (element-of-set? x (cdr set)))))
 (define (adjoin-set x set)
-      (cons x set))
+  (cons x set))
 (define (union-set set1 set2)  
-    (append set1 set2))
+  (append set1 set2))
 
 
 ;(union-set test `(1 2 3 5))
+;(define (remove-set x set)
+;  (if (element-of-set? x set)
+;      (cond ((eq? x (car set))
+;             (remove-set x (cdr set)))
+;            (else (cons (car set)
+;                        (remove-set x (cdr set)))))
+;      set))
 (define (remove-set x set)
-  (if (element-of-set? x set)
-      (cond ((eq? x (car set))
-             (remove-set x (cdr set)))
-            (else (cons (car set)
-                        (remove-set x (cdr set)))))
-      set))
+  (filter (lambda (i)
+            (not (= x i)))
+          set))
 (define (intersection-set set1 set2)
   (let ((set (union-set set1 set2)))
     (define (iter rest)
@@ -30,11 +34,10 @@
             (else
              (cons (car rest) (iter  (cdr rest))))))
     (iter set)))
- (define a `(1 2 3 4 3 2 1))
+(define a `(1 2 3 4 3 2 1))
 (define b `(3 5 6 7 1))
 (intersection-set a b)
 
 
-           
 
-  
+
