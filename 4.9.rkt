e8c8e5fcc9fcd0afab801625f7664a330e804869
pickknow <sovsov@gmail.com>
2016-11-22 16:44:40 +0800

    2016-11-22

--- 4.9.rkt
+++ 4.9.rkt
@@ -11,7 +11,7 @@
   (if (< end start )
       (proc start)
       (for-iter (+ start 1) end proc)))
-(define (for-combination exp)
+(define (for->combination exp)
   (for-iter
    (for-start exp)
    (for-end exp)
@@ -20,5 +20,55 @@
 
 (while (predicate variable)
   body)
+(define (iter predicate variable)
+  (if (predicate variable)
+      (body)
+      
 (define (while? exp) (tagged-list exp `while))
-(define while
+(define while-predicate caadr)
+(define while-variable cadadr)
+(define while-body cddr)
+
+
+(define (while->combination exp)
+  (sequence->exp
+   (lsit (list `define
+               (list `while-iter
+                     (make->if ((while-predicate exp)
+                                (while-variable exp))
+                               (sequence->exp (list (while-body exp)
+                                                    (list `while-iter))))))
+         (list `while-iter))))
+ 
+ (define (while? expr) (tagged-list? expr 'while)) 
+(define (while-condition expr) (cadr expr))
+ (define (while? exp) (tagged-list? exp 'while)) 
+ (define (while-pred exp)(cadr exp)) 
+ (define (while-actions exp) (caddr exp))   
+ (define (make-single-binding var val)(list (list var val))) 
+ (define (make-if-no-alt predicate consequent)(list 'if predicate consequent)) 
+ (define (make-combination operator operands) (cons operator operands)) 
+ (define (while->combination expr) 
+   (sequence->exp 
+    (list (list 'define  
+                (list 'while-iter) 
+                (make-if (while-condition expr)  
+                         (sequence->exp (list (while-body expr)  
+                                              (list 'while-iter))) 
+                         'true)) 
+          (list 'while-iter)))) 
+ (define (while->rec-func exp) 
+   (list 'let (make-single-binding 'while-rec '(quote *unassigned*)) 
+         (make-assignment
+          'while-rec 
+          (make-lambda
+           '() 
+           (list (make-if-no-alt  
+                  (while-pred exp) 
+                  (make-begin
+                   (append
+                    (while-actions exp) 
+                    (list (make-combination 'while-rec '())))))))) 
+         (make-combination 'while-rec '()))) 
+  
+  
--- lib/eval.rkt
+++ lib/eval.rkt
@@ -1,28 +1,4 @@
-#lang racket
-(define (lookup-variable-value exp env)
-  `ok)
-(define (make-procedure a b env)
-  `ok)
-(define (primitive-procedure? a)
-  true)
-(define (apply-primitive-procedure a b)
-  `ok)
-(define (compound-procedure? a)
-  true)
-(define (procedure-body a)
-  `ok)
-(define (extend-environment a b c)
-  `ok)
-(define (procedure-parameters a b c)
-  `ok)
-(define (procedure-environment . agrs)
-  `ok)
-(define (true? . args)
-  true)
-(define (set-variable-value! . args)
-  `ok)
-(define (define-variable! . args)
-  `ok)
+(define apply-in-underlying-scheme apply)
 (define (eval exp env)
   (cond ((self-evaluating? exp) exp)
         ((variable? exp) (lookup-variable-value exp env))
@@ -191,4 +167,154 @@
           body
           (iter (list `let (list (car lets)) (iter (cdr lets))))))
     (iter inits)))
-        
+(define (true? x)
+  (not (eq? x false)))
+(define (false? x)
+  (eq? x false))
+(define (make-procedure parameters body env)
+  (list 'procedure parameters body env))
+(define (compound-procedure? p)
+  (tagged-list? p 'procedure))
+(define (procedure-parameters p) (cadr p))
+(define (procedure-body p) (caddr p))
+(define (procedure-environment p) (cadddr p))
+(define (enclosing-environment env) (cdr env))
+(define (first-frame env) (car env))
+(define the-empty-environment '())
+(define (make-frame variables values)
+  (cons variables values))
+(define (frame-variables frame) (car frame))
+(define (frame-values frame) (cdr frame))
+(define (add-binding-to-frame! var val frame)
+  (set-car! frame (cons var (car frame)))
+  (set-cdr! frame (cons val (cdr frame))))
+(define (extend-environment vars vals base-env)
+  (if (= (length vars) (length vals))
+      (cons (make-frame vars vals) base-env)
+      (if (< (length vars) (length vals))
+          (error "Too many arguments supplied" vars vals)
+          (error "Too few arguments supplied" vars vals))))
+(define (lookup-variable-value var env)
+  (define (env-loop env)
+    (define (scan vars vals)
+      (cond ((null? vars)
+             (env-loop (enclosing-environment env)))
+            ((eq? var (car vars))
+             (car vals))
+            (else (scan (cdr vars) (cdr vals)))))
+    (if (eq? env the-empty-environment)
+        (error "Unbound variable" var)
+        (let ((frame (first-frame env)))
+          (scan (frame-variables frame)
+                (frame-values frame)))))
+  (env-loop env))
+(define (set-variable-value! var val env)
+  (define (env-loop env)
+    (define (scan vars vals)
+      (cond ((null? vars)
+             (env-loop (enclosing-environment env)))
+            ((eq? var (car vars))
+             (set-car! vals val))
+            (else (scan (cdr vars) (cdr vals)))))
+    (if (eq? env the-empty-environment)
+        (error "Unbound variable -- SET!" var)
+        (let ((frame (first-frame env)))
+          (scan (frame-variables frame)
+                (frame-values frame)))))
+  (env-loop env))
+(define (define-variable! var val env)
+  (let ((frame (first-frame env)))
+    (define (scan vars vals)
+      (cond ((null? vars)
+             (add-binding-to-frame! var val frame))
+            ((eq? var (car vars))
+             (set-car! vals val))
+            (else (scan (cdr vars) (cdr vals)))))
+    (scan (frame-variables frame)
+          (frame-values frame))))
+
+
+;4.14
+
+(define (setup-environment)
+  (let ((initial-env
+         (extend-environment (primitive-procedure-names)
+                             (primitive-procedure-objects)
+                             the-empty-environment)))
+    (define-variable! 'true true initial-env)
+    (define-variable! 'false false initial-env)
+    initial-env))
+(define the-global-environment (setup-environment))
+(define (primitive-procedure? proc)
+  (tagged-list? proc 'primitive))
+(define (primitive-implementation proc) (cadr proc))
+(define primitive-procedures
+  (list (list 'car car)
+        (list 'cdr cdr)
+        (list 'cons cons)
+        (list 'null? null?)
+        ))
+
+(define (primitive-procedure-names)
+  (map car
+       primitive-procedures))
+(define (primitive-procedure-objects)
+  (map (lambda (proc) (list 'primitive (cadr proc)))
+       primitive-procedures))
+
+(define (apply-primitive-procedure proc args)
+  (apply-in-underlying-scheme
+   (primitive-implementation proc) args))
+(define input-prompt ";;; M-Eval input:")
+(define output-prompt ";;; M-Eval value:")
+(define (driver-loop)
+  (prompt-for-input input-prompt)
+  (let ((input (read)))
+    (let ((output (eval input the-global-environment)))
+      (announce-output output-prompt)
+      (user-print output)))
+  (driver-loop))
+(define (prompt-for-input string)
+  (newline) (newline) (display string) (newline))
+(define (announce-output string)
+  (newline) (display string) (newline))
+(define (user-print object)
+  (if (compound-procedure? object)
+      (display (list 'compound-procedure
+                     (procedure-parameters object)
+                     (procedure-body object)
+                     '<procedure-env>))
+      (display object)))
+
+(driver-loop)
+;;; M-Eval input:
+(define (append x y)
+  (if (null? x)
+      y
+      (cons (car x)
+            (append (cdr x) y))))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
